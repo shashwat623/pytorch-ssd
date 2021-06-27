d64273b1af6120355f8d8ae1fb8ae8f866b14d3f
Shashwat Dalakoti <shash.dal623@gmail.com>
2021-06-27 13:02:54 +0530

    Update convert_to_caffe2_models.py

--- convert_to_caffe2_models.py
+++ convert_to_caffe2_models.py
@@ -43,7 +43,8 @@ init_net_txt_path = f"models/{net_type}_init_net.pbtxt"
 predict_net_path = f"models/{net_type}_predict_net.pb"
 predict_net_txt_path = f"models/{net_type}_predict_net.pbtxt"
 
-dummy_input = torch.randn(1, 3, 300, 300).to("cuda")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+dummy_input = torch.randn(1, 3, 300, 300).to(device)
 torch.onnx.export(net, dummy_input, model_path, verbose=False, output_names=['scores', 'boxes'])
 
 model = onnx.load(model_path)
