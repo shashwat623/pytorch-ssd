6fef148979452483f60b00f72450b1f511518a4e
Shashwat Dalakoti <shash.dal623@gmail.com>
2021-06-27 13:48:53 +0530

    Update convert_to_caffe2_models.py

--- convert_to_caffe2_models.py
+++ convert_to_caffe2_models.py
@@ -44,7 +44,7 @@ predict_net_path = f"models/{net_type}_predict_net.pb"
 predict_net_txt_path = f"models/{net_type}_predict_net.pbtxt"
 
 dummy_input = torch.randn(1, 3, 300, 300)
-print(dumm_input.dtype)
+print(dummy_input.dtype)
 torch.onnx.export(net, dummy_input, model_path, verbose=False, output_names=['scores', 'boxes'])
 
 model = onnx.load(model_path)
